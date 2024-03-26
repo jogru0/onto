aec8ada771febe042daa3a5e3e76deb37c332f99
Jonathan Gruner <jogru0@gmail.com>
2024-03-26 10:35:35 +0100

    repro test case with git2 commits

--- tests/repro.rs
+++ tests/repro.rs
@@ -6,7 +6,7 @@ use std::{
 };
 
 use anyhow::Error;
-use git2::{Commit, Index, ObjectType, Repository, Signature};
+use git2::{build::CheckoutBuilder, Commit, ObjectType, Repository};
 
 fn git(path: &str) -> Command {
     let mut command = Command::new("git");
@@ -61,6 +61,7 @@ fn git_rebase(path: &str, name: &str) -> std::result::Result<(), Error> {
 
 fn git_commit(path: &str, name: &str) -> std::result::Result<(), Error> {
     let file_name = format!("{path}/{name}");
+    dbg!(&file_name);
     File::create_new(file_name).unwrap();
 
     let status = git(path).arg("add").arg(name).spawn()?.wait()?;
@@ -92,10 +93,9 @@ fn find_last_commit(repo: &Repository) -> Result<Option<Commit>, git2::Error> {
         .map_err(|_| git2::Error::from_str("Couldn't find commit"))
 }
 
-fn git_commit_2(
-    repo: &mut Repository,
-    name: &str,
-) -> std::result::Result<git2::Oid, anyhow::Error> {
+fn git_commit_2(path: &str, name: &str) -> std::result::Result<(), anyhow::Error> {
+    let repo = git_init_2(path).unwrap();
+
     let path = repo.workdir().ok_or(Error::msg("no worktree found"))?;
     let file_name = path.join(name);
     File::create_new(file_name)?;
@@ -103,22 +103,29 @@ fn git_commit_2(
     let mut index = repo.index()?;
     index.add_path(Path::new(name))?;
     let oid = index.write_tree()?;
-    index.clear()?;
     index.write()?;
 
     let signature = repo.signature()?;
-    let maybe_parent = find_last_commit(repo)?;
+    let maybe_parent = find_last_commit(&repo)?;
     let maybe_parent_ref = maybe_parent.as_ref();
     let parents = maybe_parent_ref.as_slice();
     let tree = repo.find_tree(oid)?;
-    Ok(repo.commit(
+    let res = repo.commit(
         Some("HEAD"), //  point HEAD to our new commit
         &signature,   // author
         &signature,   // committer
         name,         // commit message
         &tree,        // tree
+        // parents
         parents,
-    )?) // parents
+    )?;
+
+    // let commit = repo.find_commit(res)?;
+
+    // repo.reset(commit.as_object(), git2::ResetType::Mixed, None)?;
+
+    // Ok(commit)
+    Ok(())
 }
 
 pub fn remove_dir_if_found(dir: &str) -> Result<(), std::io::Error> {
@@ -132,19 +139,21 @@ pub fn remove_dir_if_found(dir: &str) -> Result<(), std::io::Error> {
     })
 }
 
-fn git_what(repo: &mut Repository, name: &str) -> std::result::Result<(), anyhow::Error> {
-    // let path = repo.workdir().ok_or(Error::msg("no worktree found"))?;
-    // let file_name = path.join(name);
-    // File::create_new(file_name)?;
+// fn git_what(repo: &mut Repository, name: &str) -> std::result::Result<(), anyhow::Error> {
+//     // let path = repo.workdir().ok_or(Error::msg("no worktree found"))?;
+//     // let file_name = path.join(name);
+//     // File::create_new(file_name)?;
 
-    repo.checkout_index(None, None).unwrap();
+//     repo.in
 
-    let mut index = repo.index()?;
-    // index.add_path(Path::new(name))?;
-    // index.write()?;
+//     repo.checkout_index(None, None).unwrap();
 
-    Ok(())
-}
+//     // let mut index = repo.index()?;
+//     // index.add_path(Path::new(name))?;
+//     // index.write()?;
+
+//     Ok(())
+// }
 
 #[test]
 fn repro_issue() {
@@ -154,7 +163,7 @@ fn repro_issue() {
 
     let path = "out/repro_issue";
     remove_dir_if_found(path).unwrap();
-    let mut repo = git_init_2(path).unwrap();
+    git_init(path).unwrap();
 
     git_commit(path, "a").unwrap();
     git_commit(path, "b").unwrap();
@@ -169,26 +178,25 @@ fn repro_issue() {
     git_commit(path, "w").unwrap();
 
     git_switch_to_branch(path, main).unwrap();
-    git_what(&mut repo, "d").unwrap();
-    // git_commit(path, "d").unwrap();
-    // git_commit(path, "e").unwrap();
+    git_commit(path, "d").unwrap();
+    git_commit(path, "e").unwrap();
 
-    // git_switch_to_branch(path, branch_1).unwrap();
-    // git_commit(path, "gamma").unwrap();
-    // git_commit(path, "delta").unwrap();
+    git_switch_to_branch(path, branch_1).unwrap();
+    git_commit(path, "gamma").unwrap();
+    git_commit(path, "delta").unwrap();
 
-    // git_switch_to_branch(path, branch_2).unwrap();
-    // git_commit(path, "x").unwrap();
-    // git_commit(path, "y").unwrap();
+    git_switch_to_branch(path, branch_2).unwrap();
+    git_commit(path, "x").unwrap();
+    git_commit(path, "y").unwrap();
 
-    // git_switch_to_branch(path, branch_1).unwrap();
-    // git_rebase(path, main).unwrap();
-    // git_commit(path, "lambda").unwrap();
-    // git_commit(path, "sigma").unwrap();
-    // git_commit(path, "omega").unwrap();
+    git_switch_to_branch(path, branch_1).unwrap();
+    git_rebase(path, main).unwrap();
+    git_commit(path, "lambda").unwrap();
+    git_commit(path, "sigma").unwrap();
+    git_commit(path, "omega").unwrap();
 
-    // git_switch_to_branch(path, branch_2).unwrap();
-    // git_commit(path, "z").unwrap();
+    git_switch_to_branch(path, branch_2).unwrap();
+    git_commit(path, "z").unwrap();
 }
 
 #[test]
@@ -199,38 +207,38 @@ fn repro_issue_2() {
 
     let path = "out/repro_issue_2";
     remove_dir_if_found(path).unwrap();
-    let mut repo = git_init_2(path).unwrap();
+    git_init_2(path).unwrap();
 
-    git_commit_2(&mut repo, "a").unwrap();
-    git_commit_2(&mut repo, "b").unwrap();
-    git_commit_2(&mut repo, "c").unwrap();
+    git_commit_2(path, "a").unwrap();
+    git_commit_2(path, "b").unwrap();
+    git_commit_2(path, "c").unwrap();
 
     git_create_branch(path, branch_1).unwrap();
-    git_commit_2(&mut repo, "alpha").unwrap();
-    git_commit_2(&mut repo, "beta").unwrap();
+    git_commit_2(path, "alpha").unwrap();
+    git_commit_2(path, "beta").unwrap();
 
     git_create_branch(path, branch_2).unwrap();
-    git_commit_2(&mut repo, "v").unwrap();
-    git_commit_2(&mut repo, "w").unwrap();
+    git_commit_2(path, "v").unwrap();
+    git_commit_2(path, "w").unwrap();
 
     git_switch_to_branch(path, main).unwrap();
-    git_what(&mut repo, "d").unwrap();
-    // git_commit_2(&mut repo, "e").unwrap();
+    git_commit_2(path, "d").unwrap();
+    git_commit_2(path, "e").unwrap();
 
-    // git_switch_to_branch(path, branch_1).unwrap();
-    // git_commit_2(&mut repo, "gamma").unwrap();
-    // git_commit_2(&mut repo, "delta").unwrap();
+    git_switch_to_branch(path, branch_1).unwrap();
+    git_commit_2(path, "gamma").unwrap();
+    git_commit_2(path, "delta").unwrap();
 
-    // git_switch_to_branch(path, branch_2).unwrap();
-    // git_commit_2(&mut repo, "x").unwrap();
-    // git_commit_2(&mut repo, "y").unwrap();
+    git_switch_to_branch(path, branch_2).unwrap();
+    git_commit_2(path, "x").unwrap();
+    git_commit_2(path, "y").unwrap();
 
-    // git_switch_to_branch(path, branch_1).unwrap();
-    // git_rebase(path, main).unwrap();
-    // git_commit_2(&mut repo, "lambda").unwrap();
-    // git_commit_2(&mut repo, "sigma").unwrap();
-    // git_commit_2(&mut repo, "omega").unwrap();
+    git_switch_to_branch(path, branch_1).unwrap();
+    git_rebase(path, main).unwrap();
+    git_commit_2(path, "lambda").unwrap();
+    git_commit_2(path, "sigma").unwrap();
+    git_commit_2(path, "omega").unwrap();
 
-    // git_switch_to_branch(path, branch_2).unwrap();
-    // git_commit(path, "z").unwrap();
+    git_switch_to_branch(path, branch_2).unwrap();
+    git_commit(path, "z").unwrap();
 }
