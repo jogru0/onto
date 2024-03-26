2f564dc887d038d7d0dbf2d1e49e95e9fd42f4b2
Jonathan Gruner <jogru0@gmail.com>
2024-03-26 13:16:05 +0100

    repro test case with git2 branch creation

--- tests/repro.rs
+++ tests/repro.rs
@@ -6,7 +6,7 @@ use std::{
 };
 
 use anyhow::Error;
-use git2::{build::CheckoutBuilder, Commit, ObjectType, Repository};
+use git2::{Commit, ObjectType, Repository};
 
 fn git(path: &str) -> Command {
     let mut command = Command::new("git");
@@ -25,6 +25,18 @@ fn git_init(path: &str) -> std::result::Result<(), Error> {
 
 fn git_init_2(path: &str) -> std::result::Result<Repository, git2::Error> {
     git2::Repository::init(path)
+}
+
+fn git_create_branch_2(path: &str, name: &str) -> std::result::Result<(), Error> {
+    let repo = Repository::open(path).unwrap();
+
+    if let Some(commit) = find_last_commit(&repo)? {
+        repo.branch(name, &commit, false)?;
+    }
+
+    repo.set_head(&format!("refs/heads/{name}"))?;
+
+    Ok(())
 }
 
 fn git_create_branch(path: &str, name: &str) -> std::result::Result<(), Error> {
@@ -94,7 +106,7 @@ fn find_last_commit(repo: &Repository) -> Result<Option<Commit>, git2::Error> {
 }
 
 fn git_commit_2(path: &str, name: &str) -> std::result::Result<(), anyhow::Error> {
-    let repo = git_init_2(path).unwrap();
+    let repo = Repository::open(path).unwrap();
 
     let path = repo.workdir().ok_or(Error::msg("no worktree found"))?;
     let file_name = path.join(name);
@@ -110,7 +122,7 @@ fn git_commit_2(path: &str, name: &str) -> std::result::Result<(), anyhow::Error
     let maybe_parent_ref = maybe_parent.as_ref();
     let parents = maybe_parent_ref.as_slice();
     let tree = repo.find_tree(oid)?;
-    let res = repo.commit(
+    let _res = repo.commit(
         Some("HEAD"), //  point HEAD to our new commit
         &signature,   // author
         &signature,   // committer
@@ -119,13 +131,8 @@ fn git_commit_2(path: &str, name: &str) -> std::result::Result<(), anyhow::Error
         // parents
         parents,
     )?;
-
-    // let commit = repo.find_commit(res)?;
-
-    // repo.reset(commit.as_object(), git2::ResetType::Mixed, None)?;
-
-    // Ok(commit)
     Ok(())
+    // Ok(repo.find_commit(res)?
 }
 
 pub fn remove_dir_if_found(dir: &str) -> Result<(), std::io::Error> {
@@ -213,11 +220,11 @@ fn repro_issue_2() {
     git_commit_2(path, "b").unwrap();
     git_commit_2(path, "c").unwrap();
 
-    git_create_branch(path, branch_1).unwrap();
+    git_create_branch_2(path, branch_1).unwrap();
     git_commit_2(path, "alpha").unwrap();
     git_commit_2(path, "beta").unwrap();
 
-    git_create_branch(path, branch_2).unwrap();
+    git_create_branch_2(path, branch_2).unwrap();
     git_commit_2(path, "v").unwrap();
     git_commit_2(path, "w").unwrap();
 
